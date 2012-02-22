81610766a26b617863c5f062a810b60c00b91fd4
David.FeilSeifer <david.feilseifer@gmail.com>
2012-02-22 08:19:35 +0000

    initial commit of openni_tracker_yale package, older version of openni_tracker modified to work with the kinect arm control software; launch files to use openni_tracker to control the arms of the kinect; updated copyright notice on openni_skeleton

---
+++ sandbox/openni_tracker_yale/src/openni_tracker.cpp
@@ -0,0 +1,215 @@
+// openni_tracker.cpp
+/*
+ * openni_trakcer_yale
+ * Copyright (c) 2012, David Feil-Seifer and Tim Field
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the yale-ros-pkgs nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <tf/transform_broadcaster.h>
+#include <kdl/frames.hpp>
+
+#include <XnOpenNI.h>
+#include <XnCodecIDs.h>
+#include <XnCppWrapper.h>
+
+using std::string;
+
+xn::Context        g_Context;
+xn::DepthGenerator g_DepthGenerator;
+xn::UserGenerator  g_UserGenerator;
+
+XnBool g_bNeedPose   = FALSE;
+XnChar g_strPose[20] = "";
+
+void XN_CALLBACK_TYPE User_NewUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie) {
+	printf("New User %d\n", nId);
+
+	if (g_bNeedPose)
+		g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);
+	else
+		g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+}
+
+void XN_CALLBACK_TYPE User_LostUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie) {
+	printf("Lost user %d\n", nId);
+}
+
+void XN_CALLBACK_TYPE UserCalibration_CalibrationStart(xn::SkeletonCapability& capability, XnUserID nId, void* pCookie) {
+	printf("Calibration started for user %d\n", nId);
+}
+
+void XN_CALLBACK_TYPE UserCalibration_CalibrationEnd(xn::SkeletonCapability& capability, XnUserID nId, XnBool bSuccess, void* pCookie) {
+	if (bSuccess) {
+		printf("Calibration complete, start tracking user %d\n", nId);
+		g_UserGenerator.GetSkeletonCap().StartTracking(nId);
+	}
+	else {
+		printf("Calibration failed for user %d\n", nId);
+		if (g_bNeedPose)
+			g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);
+		else
+			g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+	}
+}
+
+void XN_CALLBACK_TYPE UserPose_PoseDetected(xn::PoseDetectionCapability& capability, XnChar const* strPose, XnUserID nId, void* pCookie) {
+    printf("Pose %s detected for user %d\n", strPose, nId);
+    g_UserGenerator.GetPoseDetectionCap().StopPoseDetection(nId);
+    g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+}
+
+void publishTransform(XnUserID const& user, XnSkeletonJoint const& joint, string const& frame_id, string const& child_frame_id) {
+    static tf::TransformBroadcaster br;
+
+    XnSkeletonJointPosition joint_position;
+    g_UserGenerator.GetSkeletonCap().GetSkeletonJointPosition(user, joint, joint_position);
+    double x = joint_position.position.X / 1000.0;
+    double y = joint_position.position.Y / 1000.0;
+    double z = joint_position.position.Z / 1000.0;
+
+    XnSkeletonJointOrientation joint_orientation;
+    g_UserGenerator.GetSkeletonCap().GetSkeletonJointOrientation(user, joint, joint_orientation);
+
+    XnFloat* m = joint_orientation.orientation.elements;
+    KDL::Rotation rotation(m[0], m[1], m[2],
+    					   m[3], m[4], m[5],
+    					   m[6], m[7], m[8]);
+    double qx, qy, qz, qw;
+    rotation.GetQuaternion(qx, qy, qz, qw);
+
+    tf::Transform transform;
+    transform.setOrigin(tf::Vector3(x, y, -z));
+    transform.setRotation(tf::Quaternion(qx, qy, qz, qw));
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), frame_id, child_frame_id));
+}
+
+void publishTransforms(const std::string& frame_id) {
+    XnUserID users[15];
+    XnUInt16 users_count = 15;
+    g_UserGenerator.GetUsers(users, users_count);
+
+    for (int i = 0; i < users_count; ++i) {
+        XnUserID user = users[i];
+        if (!g_UserGenerator.GetSkeletonCap().IsTracking(user))
+            continue;
+
+
+        publishTransform(user, XN_SKEL_HEAD,           frame_id, "head");
+        publishTransform(user, XN_SKEL_NECK,           frame_id, "neck");
+        publishTransform(user, XN_SKEL_TORSO,          frame_id, "torso");
+
+        publishTransform(user, XN_SKEL_LEFT_SHOULDER,  frame_id, "left_shoulder");
+        publishTransform(user, XN_SKEL_LEFT_ELBOW,     frame_id, "left_elbow");
+        publishTransform(user, XN_SKEL_LEFT_HAND,      frame_id, "left_hand");
+
+        publishTransform(user, XN_SKEL_RIGHT_SHOULDER, frame_id, "right_shoulder");
+        publishTransform(user, XN_SKEL_RIGHT_ELBOW,    frame_id, "right_elbow");
+        publishTransform(user, XN_SKEL_RIGHT_HAND,     frame_id, "right_hand");
+
+        publishTransform(user, XN_SKEL_LEFT_HIP,       frame_id, "left_hip");
+        publishTransform(user, XN_SKEL_LEFT_KNEE,      frame_id, "left_knee");
+        publishTransform(user, XN_SKEL_LEFT_FOOT,      frame_id, "left_foot");
+
+        publishTransform(user, XN_SKEL_RIGHT_HIP,      frame_id, "right_hip");
+        publishTransform(user, XN_SKEL_RIGHT_KNEE,     frame_id, "right_knee");
+        publishTransform(user, XN_SKEL_RIGHT_FOOT,     frame_id, "right_foot");
+    }
+}
+
+#define CHECK_RC(nRetVal, what)										\
+	if (nRetVal != XN_STATUS_OK)									\
+	{																\
+		printf("%s failed: %s\n", what, xnGetStatusString(nRetVal));\
+		return nRetVal;												\
+	}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "openni_tracker");
+    ros::NodeHandle nh;
+
+    string configFilename = ros::package::getPath("openni_tracker") + "/openni_tracker.xml";
+    XnStatus nRetVal = g_Context.InitFromXmlFile(configFilename.c_str());
+    CHECK_RC(nRetVal, "InitFromXml");
+
+    nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
+    CHECK_RC(nRetVal, "Find depth generator");
+
+	nRetVal = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
+	if (nRetVal != XN_STATUS_OK) {
+		nRetVal = g_UserGenerator.Create(g_Context);
+		CHECK_RC(nRetVal, "Find user generator");
+	}
+
+	if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON)) {
+		printf("Supplied user generator doesn't support skeleton\n");
+		return 1;
+	}
+
+    XnCallbackHandle hUserCallbacks;
+	g_UserGenerator.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);
+
+	XnCallbackHandle hCalibrationCallbacks;
+	g_UserGenerator.GetSkeletonCap().RegisterCalibrationCallbacks(UserCalibration_CalibrationStart, UserCalibration_CalibrationEnd, NULL, hCalibrationCallbacks);
+
+	if (g_UserGenerator.GetSkeletonCap().NeedPoseForCalibration()) {
+		g_bNeedPose = TRUE;
+		if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION)) {
+			printf("Pose required, but not supported\n");
+			return 1;
+		}
+
+		XnCallbackHandle hPoseCallbacks;
+		g_UserGenerator.GetPoseDetectionCap().RegisterToPoseCallbacks(UserPose_PoseDetected, NULL, NULL, hPoseCallbacks);
+
+		g_UserGenerator.GetSkeletonCap().GetCalibrationPose(g_strPose);
+	}
+
+	g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
+
+	nRetVal = g_Context.StartGeneratingAll();
+	CHECK_RC(nRetVal, "StartGenerating");
+
+	ros::Rate r(30);
+
+        
+        ros::NodeHandle pnh("~");
+        string frame_id("camera_skeleton_frame");
+        pnh.getParam("camera_frame_id", frame_id);
+                
+	while (ros::ok()) {
+		g_Context.WaitAndUpdateAll();
+		publishTransforms(frame_id);
+		r.sleep();
+	}
+
+	g_Context.Shutdown();
+	return 0;
+}
