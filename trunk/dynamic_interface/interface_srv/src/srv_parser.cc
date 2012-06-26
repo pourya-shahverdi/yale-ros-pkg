8a0a3b0361f921d5cf800007cb14a881e069e36d
david.feilseifer@gmail.com <david.feilseifer@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2012-06-26 18:16:03 +0000

    modified interface_srv to handle multiple GUIs from a single yaml file

--- trunk/dynamic_interface/interface_srv/src/srv_parser.cc
+++ trunk/dynamic_interface/interface_srv/src/srv_parser.cc
@@ -42,59 +42,64 @@ int main( int argc, char* argv[] )
 		ROS_WARN("couldn't find file: [%s]", filename.c_str());
 		return false;
 	}
-
-	YAML::Node doc;
 	YAML::Parser parser(fin);
+	YAML::Node doc;
 	parser.GetNextDocument(doc);
 
-	std::string guiname;
-	doc[0]["gui"] >> guiname;
+  for( std::size_t i = 0; i < doc.size(); i++ )
+  {
+    std::string guiname;
+    doc[i]["gui"] >> guiname;
+    ROS_INFO( "found GUI: %s", guiname.c_str() );
 	
-	// copy data into GUI response msg
-  interface_srv::GUI gui;
 
-	gui.guiname = guiname;
-	ROS_INFO( "GUIName: %s", guiname.c_str() );
+  
+	  // copy data into GUI response msg
+    interface_srv::GUI gui;
 
-	const YAML::Node& y_elements = doc[0]["elements"];
+	  gui.guiname = guiname;
+	  ROS_INFO( "GUIName: %s", guiname.c_str() );
 
-	for( unsigned int i = 0; i < y_elements.size(); i++ )
-	{
-		interface_srv::GUIElement element;	
+  	const YAML::Node& y_elements = doc[i]["elements"];
+
+	  for( unsigned int i = 0; i < y_elements.size(); i++ )
+	  {
+		  interface_srv::GUIElement element;	
 		
-		// copy label
-		y_elements[i]["label"] >> element.label;
+	  	// copy label
+  		y_elements[i]["label"] >> element.label;
 
-		// copy type 0: button 1: slider
-		std::string elem_type;
-		y_elements[i]["type"] >> elem_type;
-		if(!elem_type.compare("button"))
-			element.type = interface_srv::GUIElement::BUTTON_ID;
-		else if(!elem_type.compare("slider"))
-			element.type = interface_srv::GUIElement::SLIDER_ID;
-    else if(!elem_type.compare("stopwatch"))
-      element.type = interface_srv::GUIElement::STOPWATCH_ID;
-    else if(!elem_type.compare("countdown_timer"))
-      element.type = interface_srv::GUIElement::COUNTDOWN_ID;
-    else
-    {
-      ROS_WARN( "element type: %s unrecognized", elem_type.c_str() );
-    }
-		// copy topic
-		y_elements[i]["topic"] >> element.topic;
+		  // copy type 0: button 1: slider
+	  	std::string elem_type;
+  		y_elements[i]["type"] >> elem_type;
+		  if(!elem_type.compare("button"))
+	  		element.type = interface_srv::GUIElement::BUTTON_ID;
+  		else if(!elem_type.compare("slider"))
+			  element.type = interface_srv::GUIElement::SLIDER_ID;
+      else if(!elem_type.compare("stopwatch"))
+        element.type = interface_srv::GUIElement::STOPWATCH_ID;
+      else if(!elem_type.compare("countdown_timer"))
+        element.type = interface_srv::GUIElement::COUNTDOWN_ID;
+      else
+      {
+        ROS_WARN( "element type: %s unrecognized", elem_type.c_str() );
+      }
+  		// copy topic
+		  y_elements[i]["topic"] >> element.topic;
 
-		if (element.type == interface_srv::GUIElement::SLIDER_ID){
-			// copy min/max
-			y_elements[i]["min"] >> element.min;
-			y_elements[i]["max"] >> element.max;
-		}
+		  if (element.type == interface_srv::GUIElement::SLIDER_ID){
+	  		// copy min/max
+  			y_elements[i]["min"] >> element.min;
+			  y_elements[i]["max"] >> element.max;
+		  }
 
-		gui.elements.push_back(element);
-	}
-  response.guis.push_back(gui);
-
+		  gui.elements.push_back(element);
+	  }
+    response.guis.push_back(gui);
+  }
 	// wait for requests
 	ros::ServiceServer service = nh.advertiseService("gui_srv", gui_srv);	
 	ros::spin();
+  
 	return 0;
 }
