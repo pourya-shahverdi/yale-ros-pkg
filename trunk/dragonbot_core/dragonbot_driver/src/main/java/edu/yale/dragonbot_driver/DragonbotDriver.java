73920ff2d57de711103c740055204bf58673dc6f
david.feilseifer@gmail.com <david.feilseifer@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2012-10-18 21:09:12 +0000

    dragonbot_driver: * minor library fixes * added licence info to DragonbotDriver.java * reading config filename and debug value from parameter server * moved some stuff out of connectedNode fncs to DragonbotDriver class where they will belong

--- trunk/dragonbot_core/dragonbot_driver/src/main/java/edu/yale/dragonbot_driver/DragonbotDriver.java
+++ trunk/dragonbot_core/dragonbot_driver/src/main/java/edu/yale/dragonbot_driver/DragonbotDriver.java
@@ -1,3 +1,33 @@
+/*
+ * dragonbot_driver
+ * Copyright (c) 2012, David Feil-Seifer
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the yale-ros-pkgs nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package edu.yale.dragonbot_driver;
 
 import java.io.IOException;
@@ -20,6 +50,7 @@ import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 import org.ros.node.NodeMain;
+import org.ros.node.parameter.ParameterTree;
 import org.ros.node.topic.Publisher;
 
 /**
@@ -32,6 +63,12 @@ import org.ros.node.topic.Publisher;
  */
 
 public class DragonbotDriver extends AbstractNodeMain {
+  
+  private boolean mDebug;
+  private String mConfigFileName;
+			
+      private MCBMiniServer server = null;
+			private MCBMiniBoard board;
 
   @Override
   public GraphName getDefaultNodeName() {
@@ -40,11 +77,37 @@ public class DragonbotDriver extends AbstractNodeMain {
 
   @Override
   public void onStart(final ConnectedNode connectedNode) {
+    
+    ParameterTree params = connectedNode.getParameterTree();
+    mConfigFileName = params.getString("/dragonbot/config_file", "");
+    mDebug = params.getBoolean("/dragonbot/debug");
+
 
 		/*
 		 * Read the xml (currently as a hard-coded default file, change to filename passed in as a param)
 		 */
 		//args = new String[]{"test.xml"};
+    XMLResults results = null;
+ 		try {
+    	results = XMLUtils.parseMCBMiniConfigFile( mConfigFileName );
+ 		} catch (Exception e1) {
+     	System.err.println("Can't parse input file: "+e1.getMessage());
+  		System.exit(0);
+	  }
+
+    		/*
+		     * Initialize the server with the board list and serial port name from the xml file
+    		 */
+    		try {
+		    	// This allows us to not have a connected stack, for debugging purposes
+    			if( mDebug ) server = new DebugMCBMiniServer(results.boards);
+		    	else server = new MCBMiniServer(results.port_name, results.boards);
+          //			server = new AndroidIOIOMCBMiniServer(boards, ioio_rx_pin, ioio_tx_pin)
+		    } catch (IOException e) {
+    			// TODO Auto-generated catch block
+		    	e.printStackTrace();
+    		}
+
 
 		/*
 		 * Create a loop that loops at 50Hz forever
@@ -54,104 +117,77 @@ public class DragonbotDriver extends AbstractNodeMain {
     // down.
     connectedNode.executeCancellableLoop(new CancellableLoop() {
       private int sequenceNumber;
-			private boolean DEBUG;
-			private MCBMiniServer server = null;
-			private MCBMiniBoard board;
+
       @Override
       protected void setup() {
         sequenceNumber = 0;
-				DEBUG=false;
-		XMLResults results = null;
-		try {
-			results = XMLUtils.parseMCBMiniConfigFile( "/home/dfseifer/fuerte-ros/yale-ros-pkg/dragonbot_core/dragonbot_driver/config/example_motor_config.xml" );
-		} catch (Exception e1) {
-			System.err.println("Can't parse input file: "+e1.getMessage());
-			System.exit(0);
-		}
 
 
-		/*
-		 * Initialize the server with the board list and serial port name from the xml file
-		 */
-		try {
-			// This allows us to not have a connected stack, for debugging purposes
-			//if( DEBUG ) server = new DebugMCBMiniServer(results.boards);
-			/*else */server = new MCBMiniServer(results.port_name, results.boards);
-//			server = new AndroidIOIOMCBMiniServer(boards, ioio_rx_pin, ioio_tx_pin)
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
-		/*
-		 * Enable the board
-		 * We could also change any other parameters if we wanted to
-		 */
-		board = server.getBoards().get(0);
-		board.setChannelBParameter(ChannelParameter.ENABLED, 1);
+		    /*
+    		 * Enable the board
+		     * We could also change any other parameters if we wanted to
+    		 */
+		    board = server.getBoards().get(0);
+    		board.setChannelBParameter(ChannelParameter.ENABLED, 1);
 
-		/*
-		 * Register a handler for disable events (boards can be disabled on timeouts or fault conditions like overheating)
-		 */
-		server.addBoardDisabledEventHandler(new MCBMiniBoardDisabledHandler() {
-			@Override
-			public void handleBoardDisableEvent(MCBMiniBoard board, Channel ch) {
-				System.out.println("Board notified disable event: "+board.getId()+": "+ch);
-			}
-		});
+		    /*
+    		 * Register a handler for disable events (boards can be disabled on timeouts or fault conditions like overheating)
+		     */
+    		server.addBoardDisabledEventHandler(new MCBMiniBoardDisabledHandler() {
+		    	@Override
+    			public void handleBoardDisableEvent(MCBMiniBoard board, Channel ch) {
+		    		System.out.println("Board notified disable event: "+board.getId()+": "+ch);
+    			}
+    		});
       }
 
       @Override
       protected void loop() throws InterruptedException {
 
-
         sequenceNumber++;
 //        Thread.sleep(1000);
+		  	// Update the server, this is important and has to be done in an update loop of the application main thread
+			  server.update();
 
-			// Update the server, this is important and has to be done in an update loop of the application main thread
-			server.update();
+  			// Create the sinusoidal signal
+	  		int sin = (int)( 512 + 500 * Math.sin( (float)sequenceNumber/20f ) );
 
-			// Create the sinusoidal signal
-			int sin = (int)( 512 + 500 * Math.sin( (float)sequenceNumber/20f ) );
+		  	// Set the target position of the motor to the sinusoidal signal
+			  board.setChannelBParameter(ChannelParameter.TARGET_TICK, sin);
 
-			// Set the target position of the motor to the sinusoidal signal
-			board.setChannelBParameter(ChannelParameter.TARGET_TICK, sin);
+  			// Print out the actual tick position for the motor
+	  		System.out.println("Actual tick: " + board.getChannelBParameter(ChannelParameter.CURRENT_TICK) );
 
-			// Print out the actual tick position for the motor
-			System.out.println("Actual tick: " + board.getChannelBParameter(ChannelParameter.CURRENT_TICK) );
+		  	// Every now and then make a request for a parameter just for fun
+			  if( sequenceNumber % 200 == 0 ){
+				  server.sendRequestForResponse(board, Channel.B, Command.MOTOR_CURRENT, new MCBMiniResponseHandler() {
+					  @Override
+  					public void handleResponse(MCBMiniBoard board, Channel channel, Command command, int value) {
+	  					System.out.println("Received response to request: "+command+": "+value);
+		  			}
+			  	});
+  			} // if sequenceNUmber
 
-			// Every now and then make a request for a parameter just for fun
-			if( sequenceNumber % 200 == 0 ){
-				server.sendRequestForResponse(board, Channel.B, Command.MOTOR_CURRENT, new MCBMiniResponseHandler() {
-					@Override
-					public void handleResponse(MCBMiniBoard board, Channel channel, Command command, int value) {
-						System.out.println("Received response to request: "+command+": "+value);
-					}
-				});
-			}
+	  		/*
+		  	 * Every now and then we print out the update rates of the system (the 2nd one is more interesting, it is the response frequency of the boards
+			   * if it is ever very different from our internal one then we might have a comm problem)
+  			 */
+	  		if( sequenceNumber % 100 == 0 ){
+  				float[] fps = server.getUpdateRates(null);
+	  			System.out.println("Internal update rate: "+fps[0]+" Hz");
+		  		System.out.println("Board cycle update rate: "+fps[1]+" Hz");
+			  	board.setChannelBParameter(ChannelParameter.ENABLED, 1);
+  			} //if sequenceNumber
 
-			/*
-			 * Every now and then we print out the update rates of the system (the 2nd one is more interesting, it is the response frequency of the boards
-			 * if it is ever very different from our internal one then we might have a comm problem)
-			 */
-			if( sequenceNumber % 100 == 0 ){
-				float[] fps = server.getUpdateRates(null);
-				System.out.println("Internal update rate: "+fps[0]+" Hz");
-				System.out.println("Board cycle update rate: "+fps[1]+" Hz");
-				board.setChannelBParameter(ChannelParameter.ENABLED, 1);
-			}
-
-			// Sleep to set the update frequency
-			try {
-				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-
-
+	  		// Sleep to set the update frequency
+		  	try {
+			  	Thread.sleep(20);
+  			} catch (InterruptedException e) {
+	  			// TODO Auto-generated catch block
+		  		e.printStackTrace();
+			  } // catch
+		  } // loop
     });
   }
 }
