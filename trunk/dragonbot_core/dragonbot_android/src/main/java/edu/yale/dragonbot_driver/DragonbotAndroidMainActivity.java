d3fc31b0999ff13e460d855a9b5e6b258879e0f6
david.feilseifer@gmail.com <david.feilseifer@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2012-11-15 03:26:19 +0000

    dragonbot_android:  - basic outline of URI chooser activity  - code cleanup in Main Activity

--- trunk/dragonbot_core/dragonbot_android/src/main/java/edu/yale/dragonbot_driver/DragonbotAndroidMainActivity.java
+++ trunk/dragonbot_core/dragonbot_android/src/main/java/edu/yale/dragonbot_driver/DragonbotAndroidMainActivity.java
@@ -30,27 +30,61 @@
 
 package edu.yale.dragonbot_driver;
 
+import com.google.common.base.Preconditions;
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.AssetManager;
-
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 
 import java.io.*;
 //import java.io.InputStream;
 
+import edu.yale.dragonbot_driver.*;
+
 import android.util.Log;
 
 import mcbmini.utils.XMLUtils;
 import mcbmini.utils.XMLUtils.XMLResults;
 
+import org.ros.node.NodeConfiguration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
 public class DragonbotAndroidMainActivity extends Activity {
+
+  /**
+   * The key with which the last used {@link URI} will be stored as a
+   * preference.
+   */
+  private static final String PREFS_KEY_NAME = "URI_KEY";
+
+  private String masterUri;
+  private EditText uriText;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.uri_chooser);
+    uriText = (EditText) findViewById(R.id.master_chooser_uri);
+    // Get the URI from preferences and display it. Since only primitive types
+    // can be saved in preferences the URI is stored as a string.
+    masterUri =
+        getPreferences(MODE_PRIVATE).getString(PREFS_KEY_NAME,
+            NodeConfiguration.DEFAULT_MASTER_URI.toString());
+    uriText.setText(masterUri);
 
+/*
     // Load XML for parsing.
     AssetManager assetManager = getAssets();
 
@@ -66,34 +100,57 @@ public class DragonbotAndroidMainActivity extends Activity {
     } catch (Exception e) {
       Log.e("xml", e.getMessage());
     }
-/*
-    String s = readTextFile(xmlFileStream);
-    TextView tv = new TextView(this);
-    tv.setText(s);
-    LinearLayout ll = new LinearLayout(this);
-    ScrollView sv = new ScrollView(this);
-    ll.addView(tv);
-    sv.addView(ll);
-    setContentView(sv);
-    */
+*/
   }
 
-  private String readTextFile(InputStream inputStream) {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-    byte buf[] = new byte[1024];
-    int len;
-    try {
-        while ((len = inputStream.read(buf)) != -1) {
-            outputStream.write(buf, 0, len);
-        }
-        outputStream.close();
-        inputStream.close();
-    } catch (IOException e) {
-
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    // If the Barcode Scanner returned a string then display that string.
+    if (requestCode == 0) {
+      if (resultCode == RESULT_OK) {
+        Preconditions.checkState(intent.getStringExtra("SCAN_RESULT_FORMAT").equals("TEXT_TYPE"));
+        String contents = intent.getStringExtra("SCAN_RESULT");
+        uriText.setText(contents);
+      }
     }
-    return outputStream.toString();
-}
+  }
+
+
+  public void okButtonClicked(View unused) {
+    // Get the current text entered for URI.
+    String userUri = uriText.getText().toString();
+
+    if (userUri.length() == 0) {
+      // If there is no text input then set it to the default URI.
+      userUri = NodeConfiguration.DEFAULT_MASTER_URI.toString();
+      uriText.setText(userUri);
+      Toast.makeText(DragonbotAndroidMainActivity.this, "Empty URI not allowed.", Toast.LENGTH_SHORT).show();
+    }
+    // Make sure the URI can be parsed correctly.
+    try {
+      new URI(userUri);
+    } catch (URISyntaxException e) {
+      Toast.makeText(DragonbotAndroidMainActivity.this, "Invalid URI.", Toast.LENGTH_SHORT).show();
+      return;
+    }
+
+    // If the displayed URI is valid then pack that into the intent.
+    masterUri = userUri;
+    SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+    editor.putString(PREFS_KEY_NAME, masterUri);
+    editor.commit();
+    // Package the intent to be consumed by the calling activity.
+    Intent intent = new Intent();
+    intent.putExtra("ROS_MASTER_URI", masterUri);
+    setResult(RESULT_OK, intent);
+    finish();
+  }
+
+  public void cancelButtonClicked(View unused) {
+    setResult(RESULT_CANCELED);
+    finish();
+  }
+
 
 
 }
