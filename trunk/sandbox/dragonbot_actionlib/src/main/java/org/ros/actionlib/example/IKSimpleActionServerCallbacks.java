0b2ec8bf3d2b2679141cba55576a8e33b9ef1fa1
david.feilseifer@gmail.com <david.feilseifer@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2013-02-18 20:32:31 +0000

    dragonbot_actionlib: * lookat is not functional (accepts action message, goes to goal) * lookat does not hold still, is preemptible only because it is instantaneous; should be modified to loop until cancelled * ik not really working, but accepting actions and is preemptible

--- trunk/sandbox/dragonbot_actionlib/src/main/java/org/ros/actionlib/example/IKSimpleActionServerCallbacks.java
+++ trunk/sandbox/dragonbot_actionlib/src/main/java/org/ros/actionlib/example/IKSimpleActionServerCallbacks.java
@@ -35,28 +35,32 @@ import org.ros.message.MessageFactory;
  * A {@link SimpleActionServerCallbacks} for the Dragon example.
  */
 public class IKSimpleActionServerCallbacks
-    implements
-    SimpleActionServerCallbacks<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> {
+implements
+SimpleActionServerCallbacks<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> {
 
-DragonBotComm comm;
+  DragonBotComm comm;
+  boolean preempted = false;
 
-public IKSimpleActionServerCallbacks(DragonBotComm establishedComm)
-{
+  public IKSimpleActionServerCallbacks(DragonBotComm establishedComm)
+  {
     comm = establishedComm;
     int count =1 ;
     comm.sendNetworkDebug(count);
     while(Float.isNaN(comm.getFaceDisplayFPS()))
-	{
-	    //System.out.println("constructor loop");
-	    comm.update();
-	    count++;
-	    if(count%10000 ==0)
-		comm.sendNetworkDebug(count/10000);
-	
-	}
+    {
+      //System.out.println("constructor loop");
+      comm.update();
+      count++;
+      if(count%10000 ==0)
+        comm.sendNetworkDebug(count/10000);
+      try {
+        Thread.sleep(33);
+      }
+      catch (Exception E) {}
+    }
 
-}
- 
+  }
+
   public IKResult newResultMessage()
   {
     NodeConfiguration nc = NodeConfiguration.newPrivate();
@@ -71,114 +75,144 @@ public IKSimpleActionServerCallbacks(DragonBotComm establishedComm)
     return mf.newFromType(IKFeedback._TYPE);
   }
 
- @Override
-  public void blockingGoalCallback(IKGoal goal, SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) 
-{
 
-	int count =1;
-    comm.sendNetworkDebug(count);
-    while(Float.isNaN(comm.getFaceDisplayFPS()))
-	{
-	    comm.update();
-	    count++;
-	    if(count%10000 ==0)
-		comm.sendNetworkDebug(count/10000);
-	
-	}
-
-    if(goal.getState().equalsIgnoreCase("off"))
-	{
-		comm.sendOnOffControl(IK_CTRL.TURN_OFF);
-	}
-    else
-	{
-		comm.sendOnOffControl(IK_CTRL.TURN_ON);
-		float[] currentIK = comm.getLookatTargetCurrent();
-		currentIK[0] = (float)goal.getX();
-		currentIK[1] = (float)goal.getY();
-		currentIK[2] = (float)goal.getZ();
-		currentIK[3] = (float)goal.getTheta();
-		currentIK[4] = (float)goal.getNeck();
-		comm.sendOnOffControl(IK_CTRL.TURN_ON);
-		while(true)
-		   {
-			comm.setIKfilters((float)goal.getVel(), (float)goal.getAcc());
-			comm.sendIK(currentIK);
-			comm.update();
-		   }
-		
-	}
-
-
-}
-
-  @Override
-  public void goalCallback(
-          SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) 
+  boolean checkUpdate( SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer )
   {
-    System.out.println("GOAL CALLBACK");
-    IKGoal goal = null;
-    try
+    comm.update();
+    if( preempted )
     {
-    	goal = actionServer.acceptNewGoal();
+      System.out.println( "preempt requested" );
+      actionServer.setPreempted();
+      preempted = false;
+      return false;
     }
-    catch (RosException e){}
-    int count =1;
-    comm.sendNetworkDebug(count);
-    while(Float.isNaN(comm.getFaceDisplayFPS()))
-	  {
-	    comm.update();
-	    count++;
-	    if(count%10000 ==0)
-		  comm.sendNetworkDebug(count/10000);
-	  }
-
-    if(goal.getState().equalsIgnoreCase("off"))
-		{
-			comm.sendOnOffControl(IK_CTRL.TURN_OFF);
-		}
-
-    else
-		{
-			comm.setIKfilters((float)goal.getVel(), (float)goal.getAcc());	
-			comm.sendOnOffControl(IK_CTRL.TURN_ON);
-			float[] currentIK = new float[5];
-			if(goal.getX() > IK_RANGE.MAX_X*10000) goal.setX( IK_RANGE.MAX_X*10000);
-			if(goal.getX() < IK_RANGE.MIN_X*10000) goal.setX( IK_RANGE.MIN_X*10000);
-
-			if(goal.getY() > IK_RANGE.MAX_Y*10000) goal.setY( IK_RANGE.MAX_Y*10000);
-			if(goal.getY() < IK_RANGE.MIN_Y*10000) goal.setY( IK_RANGE.MIN_Y*10000);
-
-			if(goal.getZ() > IK_RANGE.MAX_Z*10000) goal.setZ( IK_RANGE.MAX_Z*10000);
-			if(goal.getZ() < IK_RANGE.MIN_Z*10000) goal.setZ( IK_RANGE.MIN_Z*10000);
-
-			if(goal.getTheta() > IK_RANGE.MAX_THETA*10000) goal.setTheta( IK_RANGE.MAX_THETA*10000);
-			if(goal.getTheta() < IK_RANGE.MIN_THETA*10000) goal.setTheta( IK_RANGE.MIN_THETA*10000);
-
-			if(goal.getNeck() > IK_RANGE.MAX_NECK*10000) goal.setNeck( IK_RANGE.MAX_NECK*10000);
-			if(goal.getNeck() < IK_RANGE.MIN_NECK*10000) goal.setNeck( IK_RANGE.MIN_NECK*10000);
-			currentIK[0] = (float)goal.getX();
-			currentIK[1] = (float)goal.getY();
-			currentIK[2] = (float)goal.getZ();
-			currentIK[3] = (float)goal.getTheta();
-			currentIK[4] = (float)goal.getNeck();
-			float[] actualIK = comm.getIKCurrent();
-			while(!(currentIK[0] >= actualIK[0]-.001 && currentIK[0] <= actualIK[0]+.001) ||!(currentIK[1] >= actualIK[1]-.001 && currentIK[1] <= actualIK[1]+.001) ||!(currentIK[2] >= actualIK[2]-.001 && currentIK[2] <= actualIK[2]+.001)  ||!(currentIK[3] >= actualIK[3]-.001 && currentIK[3] <= actualIK[3]+.001)  ||!(currentIK[4] >= actualIK[4]-.001 && currentIK[4] <= actualIK[4]+.001))
-			{
-				comm.sendIK(currentIK);
-				comm.update();
-				actualIK = comm.getIKCurrent();
-			}
-		}
+    return true;
   }
 
   @Override
-  public
-      void
-      preemptCallback(
-          SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) {
-    System.out.println("PREEMPT CALLBACK");
-  }
+    public void blockingGoalCallback(IKGoal goal, SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) 
+    {
+      System.out.println( "BLOCKING IK CALLBACK" );
+      int count =1;
+      comm.sendNetworkDebug(count);
+      while(Float.isNaN(comm.getFaceDisplayFPS()))
+      {
+        comm.update();
+        count++;
+        if(count%10000 ==0)
+          comm.sendNetworkDebug(count/10000);
+
+      }
+
+      if(goal.getState().equalsIgnoreCase("off"))
+      {
+        comm.sendOnOffControl(IK_CTRL.TURN_OFF);
+      }
+      else
+      {
+        comm.sendOnOffControl(IK_CTRL.TURN_ON);
+        float[] currentIK = comm.getIKCurrent();
+
+
+        Float[] currentFilters = comm.getIKfilters();
+        System.out.println( "vel: " + currentFilters[0] + " acc: " + currentFilters[1] );
+        currentIK[0] = (float)goal.getX();
+        currentIK[1] = (float)goal.getY();
+        currentIK[2] = (float)goal.getZ();
+        currentIK[3] = (float)goal.getTheta();
+        currentIK[4] = (float)goal.getNeck();
+        comm.sendOnOffControl(IK_CTRL.TURN_ON);
+        count = 0;
+        while(checkUpdate(actionServer))
+        {
+          float[] newIK = comm.getIKCurrent();
+          System.out.println( "x: " + newIK[0] + " y: " + newIK[1] + " z: " + newIK[2] + " theta: " + newIK[3] + " neck: " + newIK[4] );
+          System.out.println( "x: " + currentIK[0] + " y: " + currentIK[1] + " z: " + currentIK[2] + " theta: " + currentIK[3] + " neck: " + currentIK[4] );
+          if( count % 10000 == 0 )
+          {
+            //comm.setIKfilters((float)goal.getVel(), (float)goal.getAcc());
+            comm.sendIK(currentIK);
+
+
+          }
+          comm.update();
+          try {
+            Thread.sleep(33);
+          } catch (Exception e ) {}
+          count++;
+        }
+      }
+    }
+
+  @Override
+    public void goalCallback(
+        SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) 
+    {
+      System.out.println("GOAL CALLBACK");
+      IKGoal goal = null;
+      try
+      {
+        goal = actionServer.acceptNewGoal();
+      }
+      catch (RosException e){}
+      int count =1;
+      comm.sendNetworkDebug(count);
+      while(Float.isNaN(comm.getFaceDisplayFPS()))
+      {
+        comm.update();
+        count++;
+        if(count%10000 ==0)
+          comm.sendNetworkDebug(count/10000);
+      }
+
+      if(goal.getState().equalsIgnoreCase("off"))
+      {
+        comm.sendOnOffControl(IK_CTRL.TURN_OFF);
+      }
+
+      else
+      {
+        comm.setIKfilters((float)goal.getVel(), (float)goal.getAcc());	
+        comm.sendOnOffControl(IK_CTRL.TURN_ON);
+        float[] currentIK = new float[5];
+        if(goal.getX() > IK_RANGE.MAX_X*10000) goal.setX( IK_RANGE.MAX_X*10000);
+        if(goal.getX() < IK_RANGE.MIN_X*10000) goal.setX( IK_RANGE.MIN_X*10000);
+
+        if(goal.getY() > IK_RANGE.MAX_Y*10000) goal.setY( IK_RANGE.MAX_Y*10000);
+        if(goal.getY() < IK_RANGE.MIN_Y*10000) goal.setY( IK_RANGE.MIN_Y*10000);
+
+        if(goal.getZ() > IK_RANGE.MAX_Z*10000) goal.setZ( IK_RANGE.MAX_Z*10000);
+        if(goal.getZ() < IK_RANGE.MIN_Z*10000) goal.setZ( IK_RANGE.MIN_Z*10000);
+
+        if(goal.getTheta() > IK_RANGE.MAX_THETA*10000) goal.setTheta( IK_RANGE.MAX_THETA*10000);
+        if(goal.getTheta() < IK_RANGE.MIN_THETA*10000) goal.setTheta( IK_RANGE.MIN_THETA*10000);
+
+        if(goal.getNeck() > IK_RANGE.MAX_NECK*10000) goal.setNeck( IK_RANGE.MAX_NECK*10000);
+        if(goal.getNeck() < IK_RANGE.MIN_NECK*10000) goal.setNeck( IK_RANGE.MIN_NECK*10000);
+        currentIK[0] = (float)goal.getX();
+        currentIK[1] = (float)goal.getY();
+        currentIK[2] = (float)goal.getZ();
+        currentIK[3] = (float)goal.getTheta();
+        currentIK[4] = (float)goal.getNeck();
+        float[] actualIK = comm.getIKCurrent();
+        while(!(currentIK[0] >= actualIK[0]-.001 && currentIK[0] <= actualIK[0]+.001) ||!(currentIK[1] >= actualIK[1]-.001 && currentIK[1] <= actualIK[1]+.001) ||!(currentIK[2] >= actualIK[2]-.001 && currentIK[2] <= actualIK[2]+.001)  ||!(currentIK[3] >= actualIK[3]-.001 && currentIK[3] <= actualIK[3]+.001)  ||!(currentIK[4] >= actualIK[4]-.001 && currentIK[4] <= actualIK[4]+.001) )
+        {
+          comm.sendIK(currentIK);
+          comm.update();
+          actualIK = comm.getIKCurrent();
+          if( !checkUpdate(actionServer) ) break;
+        }
+      }
+    }
+
+  @Override
+    public
+    void
+    preemptCallback(
+        SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) {
+      System.out.println("PREEMPT CALLBACK");
+      preempted = true;
+    }
 
   private void snore() {
 
@@ -198,19 +232,19 @@ public IKSimpleActionServerCallbacks(DragonBotComm establishedComm)
    *          The action server publishing information.
    */
   private
-      void
-      publishFeedback(
-          String status,
-          SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) {
-    IKFeedback feedback = newFeedbackMessage();
-    feedback.setStatus( status );
-    actionServer.publishFeedback(feedback);
-    //for (int i = 0; i < feedback.sequence.length; i++) {
+    void
+    publishFeedback(
+        String status,
+        SimpleActionServer<IKActionFeedback, IKActionGoal, IKActionResult, IKFeedback, IKGoal, IKResult> actionServer) {
+      IKFeedback feedback = newFeedbackMessage();
+      feedback.setStatus( status );
+      actionServer.publishFeedback(feedback);
+      //for (int i = 0; i < feedback.sequence.length; i++) {
       //if (feedback.sequence[i] == 0 && i != 0) {
-       // break;
+      // break;
       //}
       //System.out.print(" " + feedback.sequence[i]);
-    //}
-  }
+      //}
+    }
 
 }
--- trunk/sandbox/dragonbot_actionlib/src/main/java/org/ros/actionlib/example/RunDragonSimpleActionServer.java
+++ trunk/sandbox/dragonbot_actionlib/src/main/java/org/ros/actionlib/example/RunDragonSimpleActionServer.java
@@ -25,91 +25,91 @@ public class RunDragonSimpleActionServer {
       // user code implementing the SimpleActionServerCallbacks interface
       ExpressionMotionSimpleActionServerCallbacks impl = new ExpressionMotionSimpleActionServerCallbacks(comm);
       VisemeSimpleActionServerCallbacks impl2 = new VisemeSimpleActionServerCallbacks(comm);
-      //LookatSimpleActionServerCallbacks impl3 = new LookatSimpleActionServerCallbacks(comm);
-      //IKSimpleActionServerCallbacks impl4 = new IKSimpleActionServerCallbacks(comm);
+      LookatSimpleActionServerCallbacks impl3 = new LookatSimpleActionServerCallbacks(comm);
+      IKSimpleActionServerCallbacks impl4 = new IKSimpleActionServerCallbacks(comm);
 
       ExpressionMotionActionSpec emSpec = new ExpressionMotionActionSpec();
       VisemeActionSpec vSpec = new VisemeActionSpec();
-      //LookatActionSpec lSpec = new LookatActionSpec();
-      //IKActionSpec ikSpec = new IKActionSpec();
+      LookatActionSpec lSpec = new LookatActionSpec();
+      IKActionSpec ikSpec = new IKActionSpec();
 
       final String nodeName1 = "ExpressionMotion_Server";
       final String nodeName2 = "Viseme_Server";
       final String nodeName3 = "Lookat_Server";
       final String nodeName4 = "IK_Server";
       final ExpressionMotionSimpleActionServer emas =
-          emSpec.buildSimpleActionServer(nodeName1, impl, true);
+        emSpec.buildSimpleActionServer(nodeName1, impl, true);
 
       final VisemeSimpleActionServer vas =
-          vSpec.buildSimpleActionServer(nodeName2, impl2, true);
-/*
+        vSpec.buildSimpleActionServer(nodeName2, impl2, true);
+
       final LookatSimpleActionServer las =
-          lSpec.buildSimpleActionServer(nodeName3, impl3, false);
+        lSpec.buildSimpleActionServer(nodeName3, impl3, true);
 
       final IKSimpleActionServer ikas =
-          ikSpec.buildSimpleActionServer(nodeName4, impl4, false);
-*/
+        ikSpec.buildSimpleActionServer(nodeName4, impl4, true);
+
       NodeConfiguration configuration = NodeConfiguration.newPrivate();
 
       NodeMainExecutor runner = DefaultNodeMainExecutor.newDefault();
 
       runner.execute(new NodeMain() {
 
-        @Override
-        public void onStart(ConnectedNode node) {
+          @Override
+          public void onStart(ConnectedNode node) {
           emas.addClientPubSub(node);
-        }
-        
-        @Override
-        public void onShutdown(Node node) {
-        }
+          }
 
-        @Override
-        public void onShutdownComplete(Node node) {
-        }
+          @Override
+          public void onShutdown(Node node) {
+          }
 
-        @Override
-        public void onError(Node node, Throwable throwable) {
-        }
+          @Override
+          public void onShutdownComplete(Node node) {
+          }
 
-        @Override
-        public GraphName getDefaultNodeName() {
-          return GraphName.of(nodeName1);
-        }
+          @Override
+          public void onError(Node node, Throwable throwable) {
+          }
+
+          @Override
+          public GraphName getDefaultNodeName() {
+            return GraphName.of(nodeName1);
+          }
       }, configuration);
-    
+
+      runner.execute(new NodeMain() {
+
+          @Override
+          public void onStart(ConnectedNode node) {
+          vas.addClientPubSub(node);
+          }
+
+          @Override
+          public void onShutdown(Node node) {
+          }
+
+          @Override
+          public void onShutdownComplete(Node node) {
+          }
+
+          @Override
+          public void onError(Node node, Throwable throwable) {
+          }
+
+          @Override
+          public GraphName getDefaultNodeName() {
+            return GraphName.of(nodeName2);
+          }
+      }, configuration);
+
       runner.execute(new NodeMain() {
 
         @Override
         public void onStart(ConnectedNode node) {
-	  vas.addClientPubSub(node);
-        }
-        
-        @Override
-        public void onShutdown(Node node) {
+        las.addClientPubSub(node);
         }
 
-        @Override
-        public void onShutdownComplete(Node node) {
-        }
-
-         @Override
-        public void onError(Node node, Throwable throwable) {
-        }
-
-       @Override
-        public GraphName getDefaultNodeName() {
-          return GraphName.of(nodeName2);
-        }
-      }, configuration);
-    
-      /*runner.execute(new NodeMain() {
-
-        @Override
-        public void onStart(ConnectedNode node) {
-	  las.addClientPubSub(node);
-        }
-        
         @Override
         public void onShutdown(Node node) {
         }
@@ -124,34 +124,34 @@ public class RunDragonSimpleActionServer {
 
         @Override
         public GraphName getDefaultNodeName() {
-          return GraphName.of(nodeName3);
+        return GraphName.of(nodeName3);
         }
-      }, configuration);*/
+        }, configuration);
 
-    /*runner.execute(new NodeMain() {
+      runner.execute(new NodeMain() {
 
-        @Override
-        public void onStart(ConnectedNode node) {
-	  ikas.addClientPubSub(node);
-        }
-        
-        @Override
-        public void onShutdown(Node node) {
-        }
+          @Override
+          public void onStart(ConnectedNode node) {
+          ikas.addClientPubSub(node);
+          }
 
-        @Override
-        public void onShutdownComplete(Node node) {
-        }
-        @Override
-        public void onError(Node node, Throwable throwable) {
-        }
+          @Override
+          public void onShutdown(Node node) {
+          }
+
+          @Override
+          public void onShutdownComplete(Node node) {
+          }
+          @Override
+          public void onError(Node node, Throwable throwable) {
+          }
 
 
-        @Override
-        public GraphName getDefaultNodeName() {
-          return GraphName.of(nodeName4);
-        }
-      }, configuration); */
+          @Override
+          public GraphName getDefaultNodeName() {
+            return GraphName.of(nodeName4);
+          }
+      }, configuration); 
     } catch (Exception e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
