6b5b6ffa8f3a5dea6f5de21b3103c43dc52f6ba5
elaine.short <elaine.short@gmail.com>
2013-04-26 18:08:54 +0000

    reorganized

--- sandbox/expeditions_year1/scripts/test.py
+++ sandbox/expeditions_year1/scripts/test.py
@@ -10,13 +10,8 @@ from interface_srv.srv import *
 
 def main():
     rospy.init_node('test_node')
-    rospy.wait_for_service('gui_srv')
-    try:
-        get_gui = rospy.ServiceProxy('gui_srv', GUIList)
-        resp = get_gui()
-        print resp.guis[0]
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+    print roslib.packages.find_node('expeditions_year1', 'intro.py')
+    
 
 if __name__ == '__main__':
     main()
