3dd6f4932a3a396eae7f0a5b6c6ba063aa1b7051
elaine.short@gmail.com <elaine.short@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2013-05-02 19:26:59 +0000

    More reorganization and changing how dialogues are specified

--- trunk/sandbox/dragonbot_teleop_dialogue/scripts/check_dialogue.py
+++ trunk/sandbox/dragonbot_teleop_dialogue/scripts/check_dialogue.py
@@ -31,84 +31,83 @@ def main():
     print "ERRORS:"
     print "++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-    for day,sessions in dialogue.items():
-        for session_name,phrases in sessions.items():
-            for phrase_name,info in phrases.items():
-                error_intro = "Error in day: " + day + " session: " + session_name + " phrase: " + phrase_name + ": "
-                if not "type" in info.keys():
-                    print error_intro + "no type given"
+    for session_name,phrases in sessions.items():
+        for phrase_name,info in phrases.items():
+            error_intro = "Error in dialogue: " + session_name + " phrase: " + phrase_name + ": "
+            if not "type" in info.keys():
+                print error_intro + "no type given"
+                continue
+            if info["type"] == "dialogue":
+                if not "items" in info.keys():
+                    print error_intro + "missing dialogue items"
                     continue
-                if info["type"] == "dialogue":
-                    if not "items" in info.keys():
-                        print error_intro + "missing dialogue items"
+                for phrase in info["items"]:
+                    if phrase not in phrases.keys():
+                        print error_intro + "item: " + phrase + " is not a phrase in the session"
                         continue
-                    for phrase in info["items"]:
-                        if phrase not in phrases.keys():
-                            print error_intro + "item: " + phrase + " is not a phrase in the session"
-                            continue
-                elif info["type"] == "statement":
-                    if not "phrase_ids" in info.keys():
-                        print error_intro + "missing phrase_ids"
+            elif info["type"] == "statement":
+                if not "phrase_ids" in info.keys():
+                    print error_intro + "missing phrase_ids"
+                    continue
+                for phrase_id in info["phrase_ids"]:
+                    if not phrase_id in valid_phrase_ids:
+                        print error_intro + "phrase_id " + phrase_id + " is not valid"
                         continue
-                    for phrase_id in info["phrase_ids"]:
-                        if not phrase_id in valid_phrase_ids:
-                            print error_intro + "phrase_id " + phrase_id + " is not valid"
-                            continue
-                elif info["type"] == "question":
-                    if not "phrase_ids" in info.keys():
-                        print error_intro + "missing phrase_ids"
+            elif info["type"] == "question":
+                if not "phrase_ids" in info.keys():
+                    print error_intro + "missing phrase_ids"
+                    continue
+                for phrase_id in info["phrase_ids"]:
+                    if not phrase_id in valid_phrase_ids:
+                        print error_intro + "phrase_id " + phrase_id + " is not valid"
                         continue
-                    for phrase_id in info["phrase_ids"]:
-                        if not phrase_id in valid_phrase_ids:
-                            print error_intro + "phrase_id " + phrase_id + " is not valid"
-                            continue
-                    if not "responses" in info.keys():
-                        print error_intro + "question missing responses"
+                if not "responses" in info.keys():
+                    print error_intro + "question missing responses"
+                    continue
+                for response in info["responses"]:
+                    if not response in phrases.keys():
+                        print  error_intro + "item: " + phrase + " is not a phrase in the session"
                         continue
-                    for response in info["responses"]:
-                        if not response in phrases.keys():
-                            print  error_intro + "item: " + phrase + " is not a phrase in the session"
-                            continue
-                    if not "terminal" in info.keys():
-                        print error_intro + "question missing terminal options"
+                if not "terminal" in info.keys():
+                    print error_intro + "question missing terminal options"
+                    continue
+                for item in info["terminal"]:
+                    if not item in info["responses"]:
+                        print error_intro + "terminal response " + item + " not in responses"
                         continue
-                    for item in info["terminal"]:
-                        if not item in info["responses"]:
-                            print error_intro + "terminal response " + item + " not in responses"
-                            continue
-                elif info["type"] == "backstory":
-                    if not "items" in info.keys():
-                        print error_intro + "missing dialogue items"
+            elif info["type"] == "backstory":
+                if not "items" in info.keys():
+                    print error_intro + "missing dialogue items"
+                    continue
+                for phrase in info["items"]:
+                    if phrase not in phrases.keys():
+                        print error_intro + "item: " + phrase + " is not a phrase in the session"
                         continue
-                    for phrase in info["items"]:
-                        if phrase not in phrases.keys():
-                            print error_intro + "item: " + phrase + " is not a phrase in the session"
-                            continue
-                elif info["type"] == "redirect":
-                    if not "goal" in info.keys():
-                        print error_intro + "redirect missing goal"
+            elif info["type"] == "redirect":
+                if not "goal" in info.keys():
+                    print error_intro + "redirect missing goal"
+                    continue
+                if not info["goal"] in phrases.keys():
+                    print error_intro + "redirect goal not in session phrases"
+                    continue
+            elif info["type"] == "wait":
+                if not "phrase_ids" in info.keys():
+                    print error_intro + "missing phrase_ids"
+                    continue
+                for phrase_id in info["phrase_ids"]:
+                    if not phrase_id in valid_phrase_ids:
+                        print error_intro + "phrase_id " + phrase_id + " is not valid"
                         continue
-                    if not info["goal"] in phrases.keys():
-                        print error_intro + "redirect goal not in session phrases"
+            elif info["type"] == "select":
+                if not "responses" in info.keys():
+                    print error_intro + "question missing responses"
+                    continue
+                for response in info["responses"]:
+                    if not response in phrases.keys():
+                        print  error_intro + "item: " + phrase + " is not a phrase in the session"
                         continue
-                elif info["type"] == "wait":
-                    if not "phrase_ids" in info.keys():
-                        print error_intro + "missing phrase_ids"
-                        continue
-                    for phrase_id in info["phrase_ids"]:
-                        if not phrase_id in valid_phrase_ids:
-                            print error_intro + "phrase_id " + phrase_id + " is not valid"
-                            continue
-                elif info["type"] == "select":
-                    if not "responses" in info.keys():
-                        print error_intro + "question missing responses"
-                        continue
-                    for response in info["responses"]:
-                        if not response in phrases.keys():
-                            print  error_intro + "item: " + phrase + " is not a phrase in the session"
-                            continue
-                else:
-                    print error_intro + "invalid type"
+            else:
+                print error_intro + "invalid type"
     print "++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 if __name__ == '__main__':
