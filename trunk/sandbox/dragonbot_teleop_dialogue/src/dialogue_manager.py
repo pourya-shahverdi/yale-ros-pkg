c0dce319d27e82a307010f212ee71c35444147a1
elaine.short@gmail.com <elaine.short@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2013-06-07 17:05:46 +0000

    EXPEDITIONS EXPERIMENT WORKING VERSION -- YALE USE THIS

--- trunk/sandbox/dragonbot_teleop_dialogue/src/dialogue_manager.py
+++ trunk/sandbox/dragonbot_teleop_dialogue/src/dialogue_manager.py
@@ -7,7 +7,6 @@ import actionlib
 
 from actionlib import *
 from actionlib.msg import *
-#from dragonbot_manager import DragonbotManager
 from tablet_manager import TabletManager
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
@@ -18,15 +17,29 @@ class NextPhraseException(Exception): pass
 
 
 class DialogueManager():
-    def __init__(self, robot_manager, tm, dialogue_name, dialogue):
-        #robot manager needs methods "say" and "stop_speech"
-        self.robot_manager = robot_manager
-        self.tm = tm
+    def __init__(self, tablet_manager, dialogue_name, dialogue):
+        self.speech_client = actionlib.SimpleActionClient('/SBPlayback_Server',SpeechPlayAction)
+
+        self.tm = tablet_manager
         self.gui_prefix = "dragon_GUI/" 
         self.dialogue = dialogue
         self.dialogue_name = dialogue_name
 
         self.seen = []
+
+    def say(self, phrase_name,interrupt = True, wait = False):
+        rospy.loginfo("Saying: " + phrase_name)
+        goal = dragon_msgs.msg.SpeechPlayGoal(phrase=phrase_name, interrupt=interrupt)
+        self.speech_client.send_goal(goal)
+        if wait:
+            rospy.loginfo("Waiting for speech server result")
+            self.speech_client.wait_for_result(rospy.Duration(60.0))
+            if not self.speech_client.get_state() == GoalStatus.SUCCEEDED:
+                rospy.logwarn("Dragonbot Manager gave up waiting for speech result. This is likely a problem.")
+
+    def stop_speech(self):
+        rospy.loginfo("Zeroing dragonbot")
+        self.speech_client.cancel_all_goals()
 
     def play_dialogue(self, item_id, interrupt = True, wait_for_finish = True):
         responses = []
@@ -34,13 +47,13 @@ class DialogueManager():
         #print "Item id: " + item_id
         #catch panic/move to next state
         if item_id == 'panic':
-            self.robot_manager.stop_speech()
+            self.stop_speech()
             raise PanicException
         if item_id == 'next_segment':
-            self.robot_manager.stop_speech()
+            self.stop_speech()
             raise NextStateException
         if item_id == 'next_phrase':
-            self.robot_manager.stop_speech()
+            self.stop_speech()
             raise NextPhraseException
         
         dialogue_item = self.dialogue[item_id]
@@ -49,7 +62,7 @@ class DialogueManager():
             responses = responses + self.play_dialogue(dialogue_item["goal"])
         elif dialogue_item["type"] == "wait":
             responses.append(item_id)
-            self.robot_manager.say(random.choice(dialogue_item["phrase_ids"]), interrupt)
+            self.say(random.choice(dialogue_item["phrase_ids"]), interrupt)
             self.seen.append(dialogue_item)
             self.tm.change("continue")
             self.tm.wait_for_press(self.gui_prefix + "continue")
@@ -78,7 +91,7 @@ class DialogueManager():
         elif dialogue_item["type"] == "question":
             gui_name = self.dialogue_name + "_" + item_id
             self.tm.change(gui_name)
-            self.robot_manager.say(random.choice(dialogue_item["phrase_ids"]), interrupt = True)
+            self.say(random.choice(dialogue_item["phrase_ids"]), interrupt = True)
             resp = self.tm.wait_for_press(self.gui_prefix + gui_name)
             try:
                 responses = responses + self.play_dialogue(resp, interrupt, wait_for_finish)
@@ -105,7 +118,7 @@ class DialogueManager():
         elif dialogue_item["type"] == "statement":
             responses.append(item_id)
             if len(dialogue_item["phrase_ids"]) > 0:
-                self.robot_manager.say(random.choice(dialogue_item["phrase_ids"]), interrupt, wait = wait_for_finish)
+                self.say(random.choice(dialogue_item["phrase_ids"]), interrupt, wait = wait_for_finish)
             self.seen.append(dialogue_item)
         print str(responses)
         return responses
