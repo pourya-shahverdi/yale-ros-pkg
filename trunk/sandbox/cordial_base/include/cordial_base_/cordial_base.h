7dad4fe5a0bb75bb81c9b720f533ff9f3826da0d
david.feilseifer@gmail.com <david.feilseifer@gmail.com@d17776de-8b95-bd4e-078a-9e139a0352bd>
2013-06-10 21:22:45 +0000

    small changes, still not ready for prime-time

--- trunk/sandbox/cordial_base/include/cordial_base_/cordial_base.h
+++ trunk/sandbox/cordial_base/include/cordial_base_/cordial_base.h
@@ -2,16 +2,24 @@
 #define CORDIAL_BASE__CORDIAL_BASE_H_
 
 #include <string>
+#include <boost/current_function.hpp>
+#include <ros/console.h>
+
+#include <dragon_msgs/VisemeAction.h>
 
 namespace cordial_base
 {
   class CordialBase
   {
+    protected:
+
+
     public:
       virtual void initialize() = 0;
       virtual std::string robot_name() = 0;
 
-      
+      virtual bool viseme_init() { ROS_WARN( "%s: not implemented", BOOST_CURRENT_FUNCTION ); return false; }
+      virtual void viseme_execute(const dragon_msgs::VisemeGoalConstPtr &goal) { ROS_WARN( "%s: not implemented", BOOST_CURRENT_FUNCTION ); }
 
 
       virtual ~CordialBase(){}
