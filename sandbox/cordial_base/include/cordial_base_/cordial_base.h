594d6e976bf39e29db4f076acce6017fe3cc527c
David.FeilSeifer <david.feilseifer@gmail.com>
2013-06-11 06:46:33 +0000

    cordial_base: * working loading of plugins from node * plugins are specified in launch files * base_plugin used as demonstration of plugin framework and extension from cordial_base class * needs comments * only works for viseme server * TODO: needs other action servers... also needs to use coridal_msgs rather than dragon_msgs

--- sandbox/cordial_base/include/cordial_base_/cordial_base.h
+++ sandbox/cordial_base/include/cordial_base_/cordial_base.h
@@ -6,17 +6,28 @@
 #include <ros/console.h>
 
 #include <dragon_msgs/VisemeAction.h>
+#include <actionlib/server/simple_action_server.h>
 
 namespace cordial_base
 {
   class CordialBase
   {
     protected:
-
+      actionlib::SimpleActionServer<dragon_msgs::VisemeAction> * viseme_as;
+      ros::NodeHandle nh_;
 
     public:
       virtual void initialize() = 0;
       virtual std::string robot_name() = 0;
+
+      void init_actionservers(ros::NodeHandle nh)
+      {
+        nh_ = nh;
+        viseme_init();
+        viseme_as = new actionlib::SimpleActionServer<dragon_msgs::VisemeAction>(nh, "Viseme_Server", boost::bind(&cordial_base::CordialBase::viseme_execute, this, _1), false);
+        viseme_as->start();
+
+      }
 
       virtual bool viseme_init() { ROS_WARN( "%s: not implemented", BOOST_CURRENT_FUNCTION ); return false; }
       virtual void viseme_execute(const dragon_msgs::VisemeGoalConstPtr &goal) { ROS_WARN( "%s: not implemented", BOOST_CURRENT_FUNCTION ); }
--- sandbox/cordial_base/src/cordial_node.cc
+++ sandbox/cordial_base/src/cordial_node.cc
@@ -1,22 +1,22 @@
 #include <ros/ros.h>
-
-#include <pluginlib/class_loader.h>
-#include <cordial_base_/cordial_base.h>
-
 #include <actionlib/server/simple_action_server.h>
+
+#include <cordial_base_/cordial_base.h>
+#include <pluginlib/class_loader.h>
 
 int main( int argc, char* argv[] )
 {
+  ros::init(argc, argv, "cordial_node");
   ros::NodeHandle nh;
   pluginlib::ClassLoader<cordial_base::CordialBase> plug_loader("cordial_base", "cordial_base::CordialBase");
   cordial_base::CordialBase* robot = NULL;
 
-
-
   // get plugin name
   std::string plugin_name = std::string("");
-
+  nh.param("cordial_robot",plugin_name, std::string(""));
   
+  actionlib::SimpleActionServer<dragon_msgs::VisemeAction> * as;
+
   // load plugin
   try
   {
@@ -24,15 +24,25 @@ int main( int argc, char* argv[] )
     robot->initialize();
 
     robot->viseme_init();
-    ROS_INFO("Base Name: %s", base->robot_name().c_str());
+    ROS_INFO("Base Name: %s", robot->robot_name().c_str());
   }
   catch(pluginlib::PluginlibException& ex)
   {
     ROS_ERROR("The plugin failed to load for some reason. Error: %s", ex.what());
   }
 
+  // initialize actionservers
+  robot->init_actionservers(nh);
+  /*
+   robot->viseme_init(); 
+  {
+    as = new actionlib::SimpleActionServer<dragon_msgs::VisemeAction>(nh, "Viseme_Server", boost::bind(&cordial_base::CordialBase::viseme_execute, robot, _1), false);
+    as->start();
+  }
+  */
   // start actionservers, if available
 
+  ros::spin();
 
   return 0;
 }
